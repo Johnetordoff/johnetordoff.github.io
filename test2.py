ac83cb41e99213bd2e4d8d933046c34a0d0b4af6
John Tordoff <johnetordoff@gmail.com>
2017-04-22 12:44:48 -0400

    test imports

--- test2.py
+++ test2.py
@@ -3,6 +3,8 @@ from browser import document, alert, html, window
 from jqueryui import jq
 from tabs import Tabs
 
+__BRYTHON__.imports()
+
 iframe = html.IFRAME()
 iframe.id = "iframe0"
 iframe.src = 'https://mfr.osf.io/render?url=https://osf.io/dawsg/?action=download%26mode=render'
