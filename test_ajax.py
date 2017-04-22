5474c7628dc2a4367b1be17a40689c42b8191afa
John Tordoff <johnetordoff@gmail.com>
2017-04-22 15:32:57 -0400

    goodie

--- test_ajax.py
+++ test_ajax.py
@@ -1,15 +1,4 @@
-from browser import document, ajax
+from browser import document
+import json
+print(json.loads(response.read()))
 
-def on_complete(req):
-    print(req.status)
-    print(req.text)
-
-
-
-req = ajax.ajax()
-req.bind('complete',on_complete)
-# send a POST request to the url
-req.open('get','https://test.com',True)
-req.set_header('content-type','application/x-www-form-urlencoded')
-# send data as a dictionary
-req.send({'x':0, 'y':1})
