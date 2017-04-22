944b8e9371c939a55f9d90a8411a1f602e2855ba
John Tordoff <johnetordoff@gmail.com>
2017-04-22 12:45:16 -0400

    test imports again

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
 from browser import document, alert, html, window
+__BRYTHON__.imports()
 
 from jqueryui import jq
 from tabs import Tabs
 
-__BRYTHON__.imports()
 
 iframe = html.IFRAME()
 iframe.id = "iframe0"
