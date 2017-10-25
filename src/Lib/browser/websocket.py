2b041612e1762b8b3ccf18ae26538004bb61c200
John Tordoff <johnetordoff@gmail.com>
2017-10-24 21:05:19 -0400

    remove brython

--- src/Lib/browser/websocket.py
+++
@@ -1,9 +0,0 @@
-from browser import window
-
-if hasattr(window, 'WebSocket'):
-    supported = True
-    WebSocket = window.WebSocket.new
-else:
-    supported = False
-    def WebSocket(*args,**kw):
-        raise NotImplementedError
